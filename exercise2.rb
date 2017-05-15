a14406ea17fe09d729b642e2a88c947495a8c050
Kathleen <kathleenmcclay@gmail.com>
2017-05-15 14:43:42 -0400

    added files

---
+++ exercise2.rb
@@ -0,0 +1,15 @@
+puts "A good tip would be 20 percent"
+puts "A good tip would be #{55 * 0.2}"
+55 + 10
+puts 55 + 10
+5 + 5
+puts " 55 + 10 "
+55 + 10
+45628 * 7839
+puts 45628 * 7839
+puts "A good tip would be" + "(55 * 0.2)"
+puts "A good tip would be" "+" "(55*0.2)"
+10 + 10
+puts 10 + 10
+puts (10 < 20 && 30 < 20) || !(10 == 11)
+my_variable = 'my_variable is now this string'
