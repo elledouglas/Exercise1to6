eb795683634235275a331e0998c2f89b2bac13c8
Kathleen <kathleenmcclay@gmail.com>
2017-05-15 15:43:41 -0400

    exercise 5

---
+++ exercise5.rb
@@ -0,0 +1,8 @@
+puts "How many pizzas would you like to order"
+
+quantity = gets.to_i
+
+puts "How many toppings would like?"
+toppings = gets.to_i
+
+puts "you have ordered #{quantity} pizzas(s) and #{toppings} toppings(s)"
